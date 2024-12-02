23f9f01b49b2deed697ea68b992eda2340efaca1
Vladimir Polozov <volodya06061991@gmail.com>
2024-12-02 19:10:55 +0300

    Commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,11 +6,15 @@ fun main(){
     var amountPurchase = readLine()!!.toDouble()
     val noDiscount: Int = 1000
     val discountHundred: Int = 10000
+    val discountVip: Double = 0.99
     val discountFivePercent: Int = 1
 
     var totalPrice: Double = if (amountPurchase > noDiscount && amountPurchase < discountHundred) amountPurchase - 100
     else if (amountPurchase > discountHundred) amountPurchase * 0.95 else amountPurchase
 
     println("Стоимость покупки с учётом скидки составит: " + totalPrice)
+    var totalPriceVip: Double = totalPrice * discountVip
+
+    println("Стоимость покупки для постоянного покупателя составит: " + totalPriceVip)
 
 }
